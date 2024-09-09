1bebd1f4faebed832cca4e4f18cd17e8658f83aa
ariana-crowell <crowella66@gmail.com>
2024-09-09 08:19:32 -0500

    Create datatypes.php

---
+++ datatypes.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    //String
+    $name = 'Coding is awesome';
+
+    //Integer
+    $name = 14;
+
+    //Float
+    $name = 14.876;
+
+    //Boolean
+
+    //Array
+    $names = array("Ariana","Alina","Joshua");
+    echo $names['2']
+
+    ?>
+</body>
+</html>
