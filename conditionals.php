0b172c204ea46a69d0b4bd39caeb09d979f8d414
ariana-crowell <crowella66@gmail.com>
2024-09-10 08:44:47 -0500

    Create conditionals.php

---
+++ conditionals.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        $x = 1;
+
+    if ($x == 1) {
+        echo "Coding is awesome!";
+    } 
+    elseif ($x == 2 ) {
+        echo "Coding is great!";
+    }
+    elseif ($x == 3 ) {
+        echo "Coding is ok";
+    }
+    elseif ($x == 4 ) {
+        echo "Coding is boring!";
+    }
+    elseif ($x == 5 ) {
+        echo "Coding is nothing!";
+    }
+    else {
+        echo "Get me out of here!";
+    }
+    ?>
+</body>
+</html>
