992d8ae827487fe45eb2d9bcecb692785c95dbae
ariana-crowell <crowella66@gmail.com>
2024-09-10 08:14:37 -0500

    Update operators.php

--- operators.php
+++ operators.php
@@ -8,7 +8,38 @@
 <body>
    
     <?php
-   
+       //Arithmetic Operators
+   echo 5**2;
+
+   //Assignment Operators
+   $x = 14;
+   $x += 20;
+   echo $x;
+
+    //Comparison Operators
+    $x = 10;
+    $y = 10;
+
+    if ($x >= $y) {
+        echo "True!";
+    }
+    else {
+        echo "False!";
+    }
+
+    //Increment/Decrement Operators
+    $x = 14;
+    echo $x--;
+    echo $x;
+
+    //Logical Operators
+    $x = 14;
+    $y = 10;
+
+    if ($x == $y xor 1 == 1) {
+        echo "True";
+    }
+    
     ?>
 </body>
 </html>
