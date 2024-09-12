6699e9c5ad701c0a4275b7c9189cafe2fad5400f
ariana-crowell <crowella66@gmail.com>
2024-09-12 07:53:59 -0500

    Create loops.php

---
+++ loops.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        //Loop
+
+        //While Loop
+        $x = 1;
+        while ($x <= 5) {
+            echo "Hi there<br>";
+            $x++;
+        }
+        //Do while loop
+        $x = 1;
+        do {
+           echo "Hey<br>";
+            $x++; 
+        }
+        while ($x <= 5);
+        //For loop
+        for ($x = 1; $x <= 10; $x++) {
+            echo "Hi<br>";
+        }
+        //Foreach loop
+        $array = array("Ariana", "Alina", "Joshua", "Joshlynn", "Danielle");
+
+        foreach ($array as $loopdata) {
+            echo "My name is ".$loopdata."<br>";
+
+        }
+    
+
+    ?>
+
+</body>
+</html>
