749d596e4467f180646c4a4dfddc313553086507
ariana-crowell <crowella66@gmail.com>
2024-09-16 08:44:34 -0500

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    
+<?php
+    
+    $x = 100;
+
+    function newCalc($x) {
+        $newnr = $x * 0.75;
+        echo "Here is 75% of what you wrote: ".$newnr;
+    }
+
+    newCalc($x);
+    echo "<br>";
+
+    $a = 10;
+    newCalc($a);
+
+?>
+
+</body>
+</html>
