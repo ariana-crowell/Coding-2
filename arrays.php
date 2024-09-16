414f136cb4d198b3b3e5c8be8d27d9036ac3682e
ariana-crowell <crowella66@gmail.com>
2024-09-16 07:38:46 -0500

    Create arrays.php

---
+++ arrays.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    
+<?php
+    $array = array("Ariana", "Alina", "Joshua", "Joshlynn", "Danielle");
+
+    echo $array[0];
+    
+?>
+
+</body>
+</html>
